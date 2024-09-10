38ee46082b2433b58f5ec386373738c9ccae37e5
YifanHan07 <yifan.han@mail.utoronto.ca>
2024-09-09 20:20:42 -0400

    add reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+    int count = 0;
+    int i = 100;
+    while (i > 0) {
+        if (i % 2 == 0) {i = i /2;}
+        else  {i = i - 1;}
+        count++;
+    }
+    System.out.println(count);
+    }
 }
